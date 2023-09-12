21299c07deb649ec46426b5c98c17f7a2013ca20
yatirastogi <63449149+yatirastogi@users.noreply.github.com>
2023-09-12 10:28:36 +0530

    Add files via upload

---
+++ Project123/src/com/amdocs/PetException.java
@@ -0,0 +1,14 @@
+package com.amdocs;
+
+public class PetException extends Exception {
+	
+	PetException() {
+	     System.out.println("Pet not available");
+	   }
+	PetException(int a)
+	{
+		System.out.print("Please enter a positive integer");
+	}
+	   
+
+}
